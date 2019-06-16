a23d29c0c2c726707715ea8abed0a968a0c4f1ff
Manujinda Wathugala <manujinda@yahoo.com>
2019-06-15 20:33:49 -0700

    Solutions for 1.1 - Is Unique

---
+++ cracking/python_3.6/01_Arrays_and_Strings/01_1_Is_Unique.py
@@ -0,0 +1,95 @@
+# Author: Manujinda Wathugala
+
+# n - Length of the string
+# c - Size of the alphabet
+
+# Using a hash set - O(min(n, c))
+def is_unique_1(str):
+    seen = set()
+
+    for chr in str:
+        if chr in seen:
+            return False
+
+        seen.add(chr)
+
+    return True
+
+
+# Using bit operations - O(min(n, c))
+# Only works for lowercase alphabetic characters
+def is_unique_2(str):
+    seen = 0
+
+    for chr in str:
+        if ord(chr) < 97 or ord(chr) > 122:
+            print('Error: Non-lowercase alphabetic character found - {}'.format(chr))
+            return None
+
+        # Index of this character in the alphabet
+        # with index of 'a' being 0 and 'z' being 25
+        char_index = ord(chr)-ord('a')
+
+        # Set the char_index bit of an integer to 1
+        flag = 1 << char_index
+
+        # Check whether we have see this character before
+        if seen & flag != 0:
+            return False
+
+        # Set the bit for this character in seen
+        seen |= flag
+
+    return True
+
+
+# Without using an additional buffer
+# Using sorting - O(nlog(n))
+def is_unique_3(str):
+    str_sorted = sorted(str)
+
+    for idx in range(len(str_sorted)-1):
+        if str_sorted[idx] == str_sorted[idx+1]:
+            return False
+
+    return True
+
+
+# Using two loops - O(n^2)
+def is_unique_4(str):
+    str_len = len(str)
+    for idx1 in range(str_len - 1):
+        for idx2 in range(idx1 + 1, str_len):
+            if str[idx1] == str[idx2]:
+                return False
+
+    return True
+
+
+# A recursive approach - O(n^2)
+def is_unique_5(str):
+    if len(str) == 1:
+        return True
+
+    for chr in str[1:]:
+        if str[0] == chr:
+            return False
+
+    return is_unique_5(str[1:])
+
+
+if __name__ == '__main__':
+
+    str = 'axbdefg'
+    print(is_unique_1(str))
+    print(is_unique_2(str))
+    print(is_unique_3(str))
+    print(is_unique_4(str))
+    print(is_unique_5(str))
+
+    str = 'axbdafg'
+    print(is_unique_1(str))
+    print(is_unique_2(str))
+    print(is_unique_3(str))
+    print(is_unique_4(str))
+    print(is_unique_5(str))
