f4dbe161418d3b85b8cb0751779afcb571c94214
Manujinda Wathugala <manujinda@yahoo.com>
2019-06-16 10:42:56 -0700

    Solutions for 1.1 - Is Unique

--- cracking/python_3.6/01_Arrays_and_Strings/01_1_Is_Unique.py
+++ cracking/python_3.6/01_Arrays_and_Strings/01_1_Is_Unique.py
@@ -18,6 +18,9 @@ def is_unique_1(str):
 
 # Using bit operations - O(min(n, c))
 # Only works for lowercase alphabetic characters
+# In Python integers can be of unlimited precision
+# Therefore, we can use this method to any size of
+# alphabet
 def is_unique_2(str):
     seen = 0
 
@@ -43,9 +46,26 @@ def is_unique_2(str):
     return True
 
 
+# Using a bit array of alphabet size
+# O(min(n, c))
+def is_unique_3(str, alph_size):
+    if len(str) > alph_size:
+        return False
+
+    seen = [False] * alph_size
+
+    for chr in str:
+        if seen[ord(chr)]:
+            return False
+
+        seen[ord(chr)] = True
+
+    return True
+
+
 # Without using an additional buffer
 # Using sorting - O(nlog(n))
-def is_unique_3(str):
+def is_unique_4(str):
     str_sorted = sorted(str)
 
     for idx in range(len(str_sorted)-1):
@@ -56,7 +76,7 @@ def is_unique_3(str):
 
 
 # Using two loops - O(n^2)
-def is_unique_4(str):
+def is_unique_5(str):
     str_len = len(str)
     for idx1 in range(str_len - 1):
         for idx2 in range(idx1 + 1, str_len):
@@ -67,7 +87,7 @@ def is_unique_4(str):
 
 
 # A recursive approach - O(n^2)
-def is_unique_5(str):
+def is_unique_6(str):
     if len(str) == 1:
         return True
 
@@ -75,7 +95,7 @@ def is_unique_5(str):
         if str[0] == chr:
             return False
 
-    return is_unique_5(str[1:])
+    return is_unique_6(str[1:])
 
 
 if __name__ == '__main__':
@@ -83,13 +103,15 @@ if __name__ == '__main__':
     str = 'axbdefg'
     print(is_unique_1(str))
     print(is_unique_2(str))
-    print(is_unique_3(str))
+    print(is_unique_3(str, 256))
     print(is_unique_4(str))
     print(is_unique_5(str))
+    print(is_unique_6(str))
 
     str = 'axbdafg'
     print(is_unique_1(str))
     print(is_unique_2(str))
-    print(is_unique_3(str))
+    print(is_unique_3(str, 256))
     print(is_unique_4(str))
     print(is_unique_5(str))
+    print(is_unique_6(str))
