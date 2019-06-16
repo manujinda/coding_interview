dd5ebc1084cac4a9ce3b3269068cddedc5da69cb
Manujinda Wathugala <manujinda@yahoo.com>
2019-06-16 11:30:36 -0700

    Solutions for 1.2 - Check Permutation

---
+++ cracking/python_3.6/01_Arrays_and_Strings/01_2_Check_Permutation.py
@@ -0,0 +1,96 @@
+# Author: Manujinda Wathugala
+# Cracking the Coding Interview
+# Chapter 1 - Arrays and Strings
+# 1.2 - Check Permutation
+
+
+# O(n)
+def check_permutation_1(str1, str2):
+
+    if len(str1) != len(str2):
+        return False
+
+    chars = {}
+
+    for chr in str1:
+        if chr in chars:
+            chars[chr] += 1
+        else:
+            chars[chr] = 1
+
+    for chr in str2:
+        if chr in chars:
+            chars[chr] -= 1
+            if chars[chr] < 0:
+                return False
+        else:
+            return False
+
+    return True
+
+
+def check_permutation_2(str1, str2, alph_size):
+    if len(str1) != len(str2):
+        return False
+
+    counts = [0] * alph_size
+
+    for chr in str1:
+        counts[ord(chr)] += 1
+
+    for chr in str2:
+        counts[ord(chr)] -= 1
+
+        if counts[ord(chr)] < 0:
+            return False
+
+    return True
+
+
+# O(n)
+def check_permutation_4(str1, str2):
+    if len(str1) != len(str2):
+        return False
+
+    char_sum = 0
+
+    for idx, chr1 in enumerate(str1):
+        char_sum += ord(chr1)
+        char_sum -= ord(str2[idx])
+
+    return char_sum == 0
+
+
+# O(n log n)
+def check_permutation_3(str1, str2):
+
+    if len(str1) != len(str2):
+        return False
+
+    str1_sorted = sorted(str1)
+    str2_sorted = sorted(str2)
+
+    #for idx, chr1 in enumerate(str1_sorted):
+    #    if chr1 != str2_sorted[idx]:
+    #        return False
+
+    #return True
+
+    return str1_sorted == str2_sorted
+
+
+
+if __name__ == '__main__':
+    str1 = 'abcde'
+    str2 = 'eabdc'
+    str3 = 'abcdd'
+
+    print(check_permutation_1(str1, str2))
+    print(check_permutation_2(str1, str2, 256))
+    print(check_permutation_3(str1, str2))
+    print(check_permutation_4(str1, str2))
+
+    print(check_permutation_1(str1, str3))
+    print(check_permutation_2(str1, str3, 256))
+    print(check_permutation_3(str1, str3))
+    print(check_permutation_4(str1, str3))
